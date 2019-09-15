ed57b36e8e13b4cdc8ca50b11c74ddd9b6d43507
Anthony Molina <antonio-molina@att.net>
2019-09-15 03:37:59 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -7,4 +7,12 @@ class MusicLibraryController
     @path = path 
     MusicImporter.new(path).import 
   end 
+  
+  def call 
+    input = gets.strip 
+    
+    if input != exit 
+      
+      puts "Welcome to your music library!" 
+      puts "To list all of your songs, enter 'list songs'."
 end 
