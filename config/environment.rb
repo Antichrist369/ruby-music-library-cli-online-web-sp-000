dd83e3ef65cf4525eba6286161cc9daa58a3d3cf
Anthony Molina <antonio-molina@att.net>
2019-09-15 03:31:38 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ Bundler.require
 module Concerns 
   module Concerns::Findable
     def find_by_name(name)
-    self.all.detect {|e| e.name == name}
-  end 
+      self.all.detect {|e| e.name == name}
+    end 
   
-  def find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name) 
-  end 
+    def find_or_create_by_name(name)
+      self.find_by_name(name) || self.create(name) 
+    end 
   end 
 end 
 
